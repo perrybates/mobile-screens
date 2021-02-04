14187826611c84a107d634cbed3808d3e6b0e7e0
Perry Bates <perryboateng08@gmail.com>
2021-02-03 22:53:51 -0200

    this is the first screen and its stil underway

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState([
+    {name:'sarkodie', key : 1},
+    {name:'joeyb', key : 2},
+    {name:'mr drew', key : 3},
+    {name:'reggie rockstone', key : 4},
+    {name:'ashad', key : 5},
+    {name:'kendrick', key : 6},
+    {name:'flick', key : 7},
+    {name:'fameye', key : 8}
+  ]);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        {people.map(person => {
+          return (
+            <View key={person.key}>
+              <Text style={styles.person}>{person.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  person: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
+  }
+  
 });
